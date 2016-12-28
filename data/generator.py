d8770bd393e09cd1fe4fe7736f1207c1bcf6f183
Juan Mendez <jmendg314@gmail.com>
2016-12-28 00:42:36 -0500

    This is still a dead project!
    In case if anybody (probably me) decides to ever recontinue this
    project, this will make my life slightly easier.

--- data/generator.py
+++ data/generator.py
@@ -1,9 +1,35 @@
 """
 Assists in generating JSON files for Guess Your Sneaker.
 """
+import os
 
 NAME_NAME = "name"
 IMAGE_NAME = "img"
+
+
+def ask_yn(question, m=1, default="y"):
+    """
+    Ask a '[y/n]' question.
+    :param question: Question.
+    :param m: Maximum number of attempts
+    :param default: Default response.
+    :return: 'y' or 'n', depending on user response.
+    """
+    if default != "y" and default != "n":
+        return "n"
+    if m < 1:
+        return default
+
+    attempt = 0
+    while attempt < m:
+        res = input("{} [y/n]: ".format(question))
+        if res != "y" and res != "n":
+            attempt += 1
+            continue
+        else:
+            return res
+
+    return default
 
 
 def create_file(dname, name, shoename, imgval):
@@ -15,13 +41,15 @@ def create_file(dname, name, shoename, imgval):
     :param name: Name of file.
     :return: Nothing.
     """
-    file = open(dname + name + ".json", "w+")
+    file = open(dname + os.path.sep + name + ".json", "w+")
     file.write("{\"" + NAME_NAME + "\":\"" + shoename + "\",\"" + IMAGE_NAME + "\":\"" + imgval + "\"}")
-    file.close();
+    file.close()
 
 
 directory = input("Target directory: ")
 initNumber = int(input("Starting number: "))
+useRelease = True if ask_yn("Use releases?") == "y" else False
+release = ""
 # Number of times the loop has passed, in order to add to initNumber to get the correct current file no.
 numberOffset = 0
 
@@ -30,6 +58,11 @@ while True:
     currentNumber = int(initNumber + numberOffset)
     print("### WRITING FOR {}".format(currentNumber))
     shoe = input("Shoe name: ")
+    release = ""
+    if useRelease:
+        release = shoe.split()[0]
+
     img = input("Image URL: ")
-    create_file(directory, str(initNumber + numberOffset), shoe.replace("\"", "\\\""), img)
+    create_file("{}{}{}".format(directory, os.path.sep, release), str(initNumber + numberOffset),
+                shoe.replace("\"", "\\\""), img)
     numberOffset += 1
